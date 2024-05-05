4b076a7f187502fa287eded595905dcc875c7494
DAVID ROBERTO RECINOS ORELLANA <105186631+Davineito@users.noreply.github.com>
2024-05-05 01:09:30 -0600

    Update README.md

--- src/main/java/com/example/ExamenProgra/dao/MovementDao.java
+++
@@ -1,7 +0,0 @@
-package com.example.ExamenProgra.dao;
-
-import com.example.ExamenProgra.entities.Movement;
-import org.springframework.data.repository.CrudRepository;
-
-public interface MovementDao extends CrudRepository<Movement, Long> {
-}
